f8d1ed90583a6f86d345182e3087e84a6d1d3eeb
Neil Thompson <neil@white-label-development.co.uk>
2014-07-11 23:29:20 +0100

    lambdas

---
+++ mrbool_5_lambda/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            List<int> myList = new List<int>();
+            myList.AddRange(new int[] { 78, 100, 92, 88, 65, 43, 70, 81, 55, 95 });
+
+
+            List<string> grades = myList.ConvertAll(
+                new Converter<int, string>(IntToGrade)
+                );
+
+
+            foreach (string grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            //delegate version
+            Console.WriteLine("--------------------------------------");
+            List<int> resultB = myList.FindAll(
+                delegate(int g)
+                {
+                    return ((g >= 80 && g < 90));
+                }
+            );
+
+            foreach (int grade in resultB)
+            {
+                Console.WriteLine(grade);
+            }
+
+            //lambda version
+            Console.WriteLine("--------------------------------------");
+
+            List<int> resultA = myList.FindAll(x => (x >= 90 && x <= 100));
+
+            foreach (int grade in resultA)
+            {
+                Console.WriteLine(grade);
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+
+        private static string IntToGrade(int input)
+        {
+            if (input > 90) return "A";
+            if (input > 80) return "B";
+            if (input > 70) return "C";
+            return "F";
+        }
+    }
+}
