ea414e5b28d9768fd006ec3883f6a969fa31e523
neil-thompson[2] <neil@treven-barn.co.uk>
2015-04-13 09:15:14 +0100

    Assignment2

--- edX-DEV204/ConsoleApplication2/ConsoleApplication2/Program.cs
+++ edX-DEV204/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -1,28 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main_mine(string[] args)
         {
 
-            int xAxisLength = 8;
-            int yAxisLength = 8;
-            char blackSquare = 'X';
-            char whiteSquare = 'O';
+            const int xAxisLength = 8;
+            const int yAxisLength = 8;
+            const char blackSquare = 'X';
+            const char whiteSquare = 'O';           
+            
 
-            int xCounter = 0;
-            int yCounter = 0;
-
-            for (int xCounter = 0; xCounter < xAxisLength; i++)
+            for (int yCounter = 0; yCounter < yAxisLength; yCounter++)
             {
-                // Code to execute.
+                bool showBlackSquare = (yCounter % 2) == 0 ;
+                for (int xCounter = 0; xCounter < xAxisLength; xCounter++)
+                {
+                    if (showBlackSquare)
+                    {
+                        Console.Write(blackSquare);
+                        showBlackSquare = false;
+                    }
+                    else
+                    {
+                        Console.Write(whiteSquare);
+                        showBlackSquare = true;
+                    }
+                }
+                Console.WriteLine();
             }
+            
+            Console.ReadLine();
         }
+
+
+        static void Main_Review1(string[] args)
+        {
+            int m, n;
+
+            for (m = 1; m <= 8; m++)
+            {
+                for (n = 1; n <= 4; n++)
+                {
+                    if (m % 2 == 0) Console.Write("OX");
+                    else Console.Write("XO");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Main_Review2(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i%2 != 0 && j%2 != 0) Console.Write("X");
+                    if (i%2 != 0 && j%2 == 0) Console.Write("O");
+                    if (i%2 == 0 && j%2 != 0) Console.Write("O");
+                    if (i%2 == 0 && j%2 == 0) Console.Write("X");
+                }
+                Console.WriteLine();
+               
+            }
+            Console.ReadLine();
+        }
+
+
     }
 }
