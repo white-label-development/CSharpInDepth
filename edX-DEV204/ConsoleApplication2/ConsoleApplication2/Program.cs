6d0922730e59ea15dab1cb2f62d6ed877af2275d
neil-thompson[2] <neil@treven-barn.co.uk>
2015-04-13 08:42:23 +0100

    Assignment2...

---
+++ edX-DEV204/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int xAxisLength = 8;
+            int yAxisLength = 8;
+            char blackSquare = 'X';
+            char whiteSquare = 'O';
+
+            int xCounter = 0;
+            int yCounter = 0;
+
+            for (int xCounter = 0; xCounter < xAxisLength; i++)
+            {
+                // Code to execute.
+            }
+        }
+    }
+}
