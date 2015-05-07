e55dead1be6232bd657014a4ed258888df7d780b
neil-thompson[2] <neil@treven-barn.co.uk>
2015-05-07 16:41:05 +0100

    assignment 10

---
+++ edX-DEV204/Module9Assignment/Mod_9_Homework/Mod_9_Homework/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Mod_9_Homework
+{
+    public class Student
+    {
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+    }
+}
