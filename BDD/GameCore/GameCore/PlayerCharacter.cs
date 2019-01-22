e5e2cc27d22ea0ae7b19c377b5aa4d09e4cc19dc
Neil Thompson <neil@white-label-development.co.uk>
2019-01-22 12:02:29 +0000

    rm example feature. add PlayerCharacter

---
+++ BDD/GameCore/GameCore/PlayerCharacter.cs
@@ -0,0 +1,17 @@
+﻿namespace GameCore
+{
+    public class PlayerCharacter
+    {
+        public int Health { get; private set; } = 100;
+        public bool IsDead { get; private set; }
+
+        public void Hit(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                IsDead = true;
+            }
+        }
+    }
+}
