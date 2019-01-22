cab49ea9baa7894704c7bbbbe90cea00223af799
Neil Thompson <neil@white-label-development.co.uk>
2019-01-22 13:17:00 +0000

    add PlayerCharacter feature, scenario and steps

---
+++ BDD/GameCore/GameCore.Specs/PlayerCharacter.feature
@@ -0,0 +1,10 @@
+﻿Feature: PlayerCharacter
+	In order to play the game
+	As a human player
+	I want my character attributes to be correctly represented
+
+
+Scenario: Taking no damage when hit does not affect health
+	Given I'm a new player	
+	When I take 0 damage
+	Then My health should now be 100
---
+++ BDD/GameCore/GameCore.Specs/PlayerCharacterSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace GameCore.Specs
+{
+    [Binding]
+    public class PlayerCharacterSteps
+    {
+
+        private PlayerCharacter _player;
+
+        [Given(@"I'm a new player")]
+        public void GivenImANewPlayer()
+        {
+            _player = new PlayerCharacter();
+
+        }
+        
+        [When(@"I take 0 damage")]
+        public void WhenITakeDamage()
+        {
+            _player.Hit(0);
+        }
+
+        [Then(@"My health should now be 100")]
+        public void ThenMyHealthShouldNowBe100()
+        {
+            Assert.Equal(100, _player.Health);
+        }
+    }
+}
